58d3ee49ac6f1e88395b14baaf4abdef15894875
Ronny Trommer <ronny@opennms.org>
2022-01-11 13:44:28 +0100

    Update to Ubuntu:impish-20220105.b122

--- bzflag/version-lock.sh
+++ bzflag/version-lock.sh
@@ -3,5 +3,5 @@ set -u -o pipefail
 
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
-export BASE_IMAGE="quay.io/labmonkeys/ubuntu:impish-20211102.b72"
+export BASE_IMAGE="quay.io/labmonkeys/ubuntu:impish-20220105.b122"
 export BZFS_VERSION="2.4.20-1"
