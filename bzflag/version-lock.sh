ccfbe7074391e99f48fd0d298dc6a75804c0b260
Ronny Trommer <ronny@opennms.org>
2022-10-07 17:05:41 +0200

    Update BZflag OS base image

--- bzflag/version-lock.sh
+++ bzflag/version-lock.sh
@@ -3,6 +3,6 @@ set -u -o pipefail
 
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
-export BASE_IMAGE="quay.io/labmonkeys/ubuntu:jammy-20220801.b187"
+export BASE_IMAGE="quay.io/labmonkeys/ubuntu:jammy-20221003.b213"
 export CA_CERT_VERSION="20211016"
 export BZFS_VERSION="2.4.22-1build1"
